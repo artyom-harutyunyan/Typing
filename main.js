17986a0b1cd9c12459f7d440ec8065d1cd333272
artyom-harutyunyan <48210362+artyom-harutyunyan@users.noreply.github.com>
2019-05-23 20:58:46 +0500

    a

---
+++ main.js
@@ -0,0 +1,25 @@
+var arr = [];
+var wrap = document.getElementsByClassName('element')[0];
+var line = document.getElementsByClassName('lines')[0];
+var button = document.getElementsByClassName('button')[0];
+// function () {
+//     arr.push({
+//         line: line,
+//         button: button,
+//     });    
+//     capture(e);
+// }
+var keys = Math.round(Math.random()* 100);
+var keysArr = [];
+console.log(keys);
+function generate() {
+    // console.log(arr);
+    
+        var NewLine = document.createElement('div');
+        NewLine.className = 'lines';
+        wrap.appendChild(NewLine);
+        var newButton = document.createElement('span');
+        newButton.className = 'button';
+        NewLine.appendChild(newButton);
+        generate();
+}
