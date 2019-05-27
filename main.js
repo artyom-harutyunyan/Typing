3ef0ce4da3f651122f77c139c99115d5d650e2c8
artyom-harutyunyan <48210362+artyom-harutyunyan@users.noreply.github.com>
2019-05-27 17:13:17 +0500

    Update main.js

--- main.js
+++ main.js
@@ -2,25 +2,19 @@ var arr = [];
 var wrap = document.getElementsByClassName('element')[0];
 var line = document.getElementsByClassName('lines')[0];
 var button = document.getElementsByClassName('button')[0];
-// function () {
-//     arr.push({
-//         line: line,
-//         button: button,
-//     });    
-//     capture(e);
-// }
+
 var keys = Math.round(Math.random()* 100);
 var keysArr = [];
 console.log(keys);
 function generate() {
-    // console.log(arr);
+    // console.log(arr)
     
-        var NewLine = document.createElement('div');
-        NewLine.className = 'lines';
-        wrap.appendChild(NewLine);
-        var newButton = document.createElement('span');
-        newButton.className = 'button';
-        newButton.innerHTML = keys;
-        NewLine.appendChild(newButton);
-        generate();
+        // var NewLine = document.createElement('div');
+        // NewLine.className = 'lines';
+        // wrap.appendChild(NewLine);
+        // var newButton = document.createElement('span');
+        // newButton.className = 'button';
+        // newButton.innerHTML = keys;
+        // NewLine.appendChild(newButton);
+        // generate();
 }
