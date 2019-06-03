e3bde43b36c93194d12717be58f8b90ba4001881
artyom-harutyunyan <48210362+artyom-harutyunyan@users.noreply.github.com>
2019-06-03 20:17:03 +0500

    V 1.2

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ var wrap = document.getElementsByClassName('element')[0];
 var line = document.getElementsByClassName('lines')[0];
 var button = document.getElementsByClassName('button')[0];
 var offStartButton = document.getElementById('startButton');
+var count = document.getElementsByClassName('counter')[0];
+var counter = 0;
+count.innerHTML = "Your score : " +  counter;
+
 
 function generate_random_letter(e){
     let random_el;
@@ -11,6 +15,7 @@ function generate_random_letter(e){
     return random_el;
 }
 function generate() {
+    // setTimeout(function(){ time.innerHTML = "Your score : " + counter++; }, 50);
     // var random = Math.round(Math.random() * 100);
     // wrap.innerHTML = '';
     var x = generate_random_letter(1);
@@ -32,10 +37,18 @@ function generate() {
         if(event.keyCode == x) {
             newLine.className += ' line_up';
             newButton.className += ' correct_button';
+            count.innerHTML = "Your score : " + counter++;
             generate();
             // alert('ok')
         }else {
             newButton.className += ' wrong_button';
+            count.innerHTML = "Your score : " + counter--;
+            if(counter <= -1) {
+                alert("You lose");
+                setTimeout(function(){
+                    window.location.reload(1);
+                 }, 100);
+            }
             // alert('no')
         }
     }
