ddf1526e29253fc256ae17348e1f39632c10330c
artyom-harutyunyan <48210362+artyom-harutyunyan@users.noreply.github.com>
2019-06-03 11:32:55 +0500

    maket done

--- main.js
+++ main.js
@@ -1,20 +1,37 @@
-var arr = [];
 var wrap = document.getElementsByClassName('element')[0];
 var line = document.getElementsByClassName('lines')[0];
 var button = document.getElementsByClassName('button')[0];
+var offStartButton = document.getElementById('startButton');
 
-var keys = Math.round(Math.random()* 100);
-var keysArr = [];
-console.log(keys);
-function generate() {
-    // console.log(arr)
-    
-        // var NewLine = document.createElement('div');
-        // NewLine.className = 'lines';
-        // wrap.appendChild(NewLine);
-        // var newButton = document.createElement('span');
-        // newButton.className = 'button';
-        // newButton.innerHTML = keys;
-        // NewLine.appendChild(newButton);
-        // generate();
+function generate_random_letter(e){
+    let random_el;
+    for(let i = 0; i < e; i++) {
+        random_el = Math.floor((Math.random() * 25) + 97);
+    }
+    return random_el;
 }
+function generate() {
+    // var random = Math.round(Math.random() * 100);
+    var x = generate_random_letter(1);
+    let random_string = '';
+
+    var newLine = document.createElement('div');
+    newLine.className = 'lines';
+    wrap.appendChild(newLine);
+
+    var newButton = document.createElement('span');
+    newButton.className = 'button';
+    newButton.innerHTML = random_string += String.fromCharCode(x);
+    newLine.appendChild(newButton);
+
+    // offStartButton.style.display = 'none';
+    console.log(x);
+  
+    document.onkeypress = function presset(event) {
+        if(event.keyCode == x) {
+            alert('ok')
+        }else alert('no')
+    }
+} 
+
+
